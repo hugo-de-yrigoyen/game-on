9407c8a0a47ce7afda0b833da29175215deebc0d
Hugo de Yrigoyen <hugo.deyrigoyen@gmail.com>
2023-07-27 09:28:41 +0200

    fix: modal can now be closed

--- front/js/modal.js
+++ front/js/modal.js
@@ -11,13 +11,20 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const close = document.querySelector(".close");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+// close modal event
+close.addEventListener("click", closeModal);
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
 
-
+// close modal form
+function closeModal() {
+  modalbg.style.display = "none";
+}
